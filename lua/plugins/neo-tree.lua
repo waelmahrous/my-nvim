288d836c2e4e2694e911271496b1488aa1e86846
Wael Mahrous <wael.mahrous@svep.se>
2025-08-19 15:15:05 +0200

    Use nvimtree instead of neotree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,7 @@
+-- ~/.config/nvim/lua/plugins/neo-tree.lua
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+}
---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,14 @@
+-- ~/.config/nvim/lua/plugins/nvim-tree.lua
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+      require("nvim-tree").setup({
+        -- Add your preferences here
+      })
+    end,
+  },
+}
