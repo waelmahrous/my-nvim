04c0301ef1f4f790ece7b03522032553686845b9
Wael Mahrous <wael.mahrous@svep.se>
2025-08-21 15:05:10 +0200

    Force tree to have same color as editor bg

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -9,15 +9,35 @@ return {
       vim.g.loaded_netrwPlugin = 1
       vim.opt.termguicolors = true
 
+      local function set_tree_highlights()
+        vim.api.nvim_set_hl(0, "NvimTreeNormal",      { link = "Normal" })
+        vim.api.nvim_set_hl(0, "NvimTreeNormalNC",    { link = "Normal" })
+        vim.api.nvim_set_hl(0, "NvimTreeNormalFloat", { link = "Normal" })
+        vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { link = "Normal" })
+        vim.api.nvim_set_hl(0, "NvimTreeCursorLine",  { link = "Substitute" })
+      end
+
       require("nvim-tree").setup({
-        view = {
-          width = 40,
+        view = { width = 40 },
+
+        update_focused_file = {
+          enable = true,
+          update_cwd = true,
         },
+
+        on_attach = function(bufnr)
+          local api = require("nvim-tree.api")
+          -- keep default mappings
+          api.config.mappings.default_on_attach(bufnr)
+          -- then apply highlights
+          set_tree_highlights()
+        end,
       })
 
+      -- apply once at startup
+      set_tree_highlights()
     end,
 
-    -- Custom mappings
     keys = {
       { "<leader>o", "<cmd>NvimTreeToggle<cr>", desc = "Open nvimtree" },
     },
