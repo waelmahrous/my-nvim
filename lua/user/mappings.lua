c76d66e2b6d595c33cc3aca3afbf4d0331334df3
Wael Mahrous <wael.mahrous@svep.se>
2025-09-10 09:58:05 +0200

    Center on jump (basic)

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -1,6 +1,7 @@
 -- Some custom mappings to keep in one place
 vim.keymap.set("n", "<C-o>", "<C-o>zz", { desc = "Jump back centered", silent = true })
 vim.keymap.set("n", "<C-i>", "<C-i>zz", { desc = "Jump forward centered", silent = true })
+vim.keymap.set("n", "G", "Gzz", { desc = "Jump EOF centered", silent = true })
 
 -- (optional) Center after search next/prev
 vim.keymap.set("n", "n", "nzzzv", { desc = "Search next centered", silent = true })
