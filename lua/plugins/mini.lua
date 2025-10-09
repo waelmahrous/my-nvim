8f5c730c628101183e81ce98b7646c85f1e84213
Wael Mahrous <wael.mahrous@svep.se>
2025-10-09 10:24:14 +0200

    Add mini plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,11 +1,15 @@
 -- Mini plugins
 return {
-  {
-    "nvim-mini/mini.nvim",
-    version = "*",
-    config = function()
-      -- Example: activate only the modules you need:
-      require("mini.ai").setup({})
-    end,
-  },
+    {
+        "nvim-mini/mini.nvim",
+        version = "*",
+        config = function()
+            require("mini.animate").setup {
+                cursor = { enable = false },
+                scroll = { enable = false },
+            }
+            require("mini.ai").setup {}
+            require("mini.surround").setup {}
+        end,
+    },
 }
