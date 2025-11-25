2d9e2867f7fc9ae4f4c33616f99c8f8776f42f21
Wael Mahrous <wael.mahrous@svep.se>
2025-11-25 17:19:09 +0100

    Better inline diagnostics

---
+++ lua/plugins/diagnostics.lua
@@ -0,0 +1,20 @@
+return {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+        require("tiny-inline-diagnostic").setup {
+            preset = "nonerdfont",
+
+            options = {
+                add_messages = {
+                    display_count = true,
+                },
+                multilines = {
+                    enabled = true,
+                },
+            },
+        }
+        vim.diagnostic.config { virtual_text = false } -- Disable Neovim's default virtual text diagnostics
+    end,
+}
