0fda0bfd49ee27f34cc90851feddbfb763f325ca
Wael Mahrous <wael.mahrous@svep.se>
2025-11-25 15:08:11 +0100

    Big tabs yo

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,15 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+-- Filetypes that should still use spaces
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "lua", "python", "yaml", "markdown", "json" },
+    callback = function()
+        vim.opt_local.expandtab = true
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.softtabstop = 4
+        vim.opt_local.tabstop = 4
+    end,
+})
